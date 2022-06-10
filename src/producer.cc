4b6763b48d4d28f4d9bf55ca6fb288b80bdcf297
Jeremy Hsu <jeremyhsu1@gmail.com>
2022-06-10 00:27:34 -0700

    Small client bug

--- src/producer.cc
+++ src/producer.cc
@@ -1,6 +1,7 @@
 #define _XOPEN_SOURCE
 
 #include <iostream>
+#include <cassert>
 
 #include <unistd.h>
 
@@ -57,46 +58,45 @@ int main() {
       printf("fgets failed\n");
       break;
     }
-  }
 
-  send_message.payload = read_buffer;
-  send_message.length = strlen(read_buffer);
+    send_message.payload = read_buffer;
+    send_message.length = strlen(read_buffer);
 
-  if (send_message.length > 1) {
-    // First send header to the server
-    int r = send(client_socket, &send_message, sizeof(Packet), 0);
-    if (r == -1) {
-      printf("Failed to send message header\n");
-      exit(1);
-    }
+    if (send_message.length > 1) {
+      // First send header to the server
+      int r = send(client_socket, &send_message, sizeof(Packet), 0);
+      if (r == -1) {
+        printf("Failed to send message header\n");
+        exit(1);
+      }
 
-    // If header is sent successfully, then send the payload
-    r = send(client_socket, send_message.payload, send_message.length, 0);
-    if (r == -1) {
-      printf("Failed to send message payload\n");
-      exit(1);
-    }
+      // If header is sent successfully, then send the payload
+      r = send(client_socket, send_message.payload, send_message.length, 0);
+      if (r == -1) {
+        printf("Failed to send message payload\n");
+        exit(1);
+      }
 
-    // Always wait for a response from the server, even if it just an OK
-    ssize_t len = recv(client_socket, &recv_message, sizeof(Packet), 0);
-    if (len > 0) {
-      if (recv_message.status == OK_DONE && recv_message.length > 0) {
-        unsigned nbytes = recv_message.length;
-        char payload[nbytes + 1];
-        len = recv(client_socket, payload, nbytes, 0);
-
-        if (len > 0) {
-          payload[nbytes] = '\0';
-          printf("%s\n", payload);
+      // Always wait for a response from the server, even if it just an OK
+      ssize_t len = recv(client_socket, &recv_message, sizeof(Packet), 0);
+      if (len > 0) {
+        if (recv_message.status == OK_DONE && recv_message.length > 0) {
+          unsigned nbytes = recv_message.length;
+          char payload[nbytes + 1];
+          len = recv(client_socket, payload, nbytes, 0);
+          if (len > 0) {
+            payload[nbytes] = '\0';
+            printf("%s\n", payload);
+          }
         }
-      }
-    } else {
-      if (len < 0) {
-        printf("Failed to receive message\n");
       } else {
-        printf("Server closed connection\n");
+        if (len < 0) {
+          printf("Failed to receive message\n");
+        } else {
+          printf("Server closed connection\n");
+        }
+        exit(1);
       }
-      exit(1);
     }
   }
 
