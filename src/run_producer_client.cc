5546441e07963e2df2239be263a6da91109a571b
Jeremy Hsu <jeremyhsu1@gmail.com>
2022-08-11 01:10:40 -0700

    Switch from Unix sockets to concurrent Internet sockets

--- src/run_producer_client.cc
+++ src/run_producer_client.cc
@@ -8,23 +8,62 @@
 #include "common.h"
 #include "producer_client.h"
 
+#include <arpa/inet.h>
+
+#define PORT 8888
+
 int main() {
-  // Tbh we can just make the transactions here for now
-  ProducerClient* prod = new ProducerClient;
-  prod->connect_to_server();
-  if (prod->client_socket < 0) {
-    exit(1);
+  // // Tbh we can just make the transactions here for now
+  // ProducerClient* prod = new ProducerClient;
+  // prod->connect_to_server();
+  // if (prod->client_socket < 0) {
+  //   exit(1);
+  // }
+
+  // prod->init_transactions();
+  // prod->begin_transaction();
+  // for (size_t i = 0; i < 10; ++i) {
+  //   prod->send_record(i);
+  // }
+  // prod->commit_transaction();
+  // prod->close_producer();
+  // prod->close_connection();
+
+  // delete prod;
+  // return 0;
+
+  struct sockaddr_in server_addr;
+  char buf[1024] = {0};
+  std::string message = "Hello from client!";
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0) {
+    perror("Error creating socket\n");
+    return -1;
   }
 
-  prod->init_transactions();
-  prod->begin_transaction();
-  for (size_t i = 0; i < 10; ++i) {
-    prod->send_record(i);
-  }
-  prod->commit_transaction();
-  prod->close_producer();
-  prod->close_connection();
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(PORT);
 
-  delete prod;
+  // Convert IPv4 and IPv6 address from texxt to binary
+  int r = inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+  if (r <= 0) {
+    perror("Invalid address! Address not supported\n");
+    return -1;
+  }
+
+  int client_fd = connect(sock, (struct sockaddr*)&server_addr,
+                          sizeof(struct sockaddr));
+  if (client_fd < 0) {
+    perror("Connection failed\n");
+    return -1;
+  }
+
+  send(sock, message.c_str(), strlen(message.c_str()), 0);
+  ssize_t valread = read(sock, buf, 1024);
+  printf("From server: %s\n", buf);
+
+  close(client_fd);
   return 0;
+
 }
--- src/server.cc
+++ src/server.cc
@@ -6,6 +6,13 @@
 #include "broker.h"
 #include "common.h"
 
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+#define PORT 8888
+#define MAX_CLIENTS 30
+
 BrokerManager* broker = nullptr;
 
 void Server::setup() {
@@ -120,26 +127,157 @@ void Server::handle_client(const int client_socket,
 }
 
 int main() {
+  // broker = new BrokerManager();
+  // Server* server = new Server();
+  // broker->server = server;
+  // server->setup();
+  // if (server->server_socket < 0) {
+  //   exit(1);
+  // }
+  // printf("Server socket %d waiting for a connection...\n",
+  //        server->server_socket);
+
+  // sockaddr_un remote;
+  // socklen_t len = sizeof(remote);
+  // int client_socket = accept(server->server_socket, (sockaddr*)&remote, &len);
+  // if (client_socket == -1) {
+  //   printf("Failed to accept a new connection\n");
+  //   exit(1);
+  // }
+
+  // server->handle_client(client_socket, PRODUCER);
+  // delete server;
+  // delete broker;
+  // return 0;
+
   broker = new BrokerManager();
   Server* server = new Server();
   broker->server = server;
-  server->setup();
-  if (server->server_socket < 0) {
-    exit(1);
-  }
-  printf("Server socket %d waiting for a connection...\n",
-         server->server_socket);
 
-  sockaddr_un remote;
-  socklen_t len = sizeof(remote);
-  int client_socket = accept(server->server_socket, (sockaddr*)&remote, &len);
-  if (client_socket == -1) {
-    printf("Failed to accept a new connection\n");
-    exit(1);
+  int opt = 1;
+  int new_socket;
+  int client_sockets[MAX_CLIENTS] = {0};
+  int activity;
+  int valread;
+  int sd;
+  int max_sd;
+
+  struct sockaddr_in addr;
+  char buf[1024];
+
+  // Set of socket descriptors
+  fd_set readfds;
+
+  // Some message
+  std::string message = "ECHO Daemon v1.0\r\n";
+
+  int master_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (master_socket == 0) {
+    perror("Failed to create master socket\n");
+    exit(EXIT_FAILURE);
   }
 
-  server->handle_client(client_socket, PRODUCER);
+  // Set the master socket to allow multiple connections
+  int r = setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char*)&opt,
+                     sizeof(int));
+  if (r < 0) {
+    perror("Failed on setsockopt\n");
+    exit(EXIT_FAILURE);
+  }
+
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(PORT);
+
+  // Bind socket to localhost port 8888
+  r = bind(master_socket, (struct sockaddr*)&addr, sizeof(addr));
+  if (r < 0) {
+    perror("bind failed\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("Listening on port %d\n", PORT);
+
+  // Specify max of 3 pending connections for the master socket at any point
+  r = listen(master_socket, 3);
+  if (r < 0) {
+    perror("listen failed\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Accept incoming connections
+  size_t addrlen = sizeof(addr);
+  while (true) {
+    // Clear socket set
+    FD_ZERO(&readfds);
+
+    // Add master socket to set
+    FD_SET(master_socket, &readfds);
+    max_sd = master_socket;
+
+    // Add child sockets to set
+    for (size_t i = 0; i < MAX_CLIENTS; ++i) {
+      sd = client_sockets[i];
+      if (sd > 0) {
+        FD_SET(sd, &readfds);
+      }
+      max_sd = std::max(max_sd, sd);
+    }
+
+    // Wait indefinitely for activity from at least one of the sockets
+    activity = select(max_sd + 1, &readfds, nullptr, nullptr, nullptr);
+    if (activity < 0 && errno != EINTR) {
+      perror("select error\n");
+    }
+
+    // Handle new connection
+    if (FD_ISSET(master_socket, &readfds)) {
+      new_socket = accept(master_socket, (struct sockaddr*)&addr,
+                          (socklen_t*)&addrlen);
+      if (new_socket < 0) {
+        perror("accept error\n");
+        exit(EXIT_FAILURE);
+      }
+
+      printf("New connection with socket fd %d, IP %s, and port number %d\n",
+             new_socket, inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+
+      r = send(new_socket, message.c_str(), strlen(message.c_str()), 0);
+      if (r < 0) {
+        perror("Error sending connection message\n");
+      }
+
+      // Add new socket to socket array
+      for (size_t i = 0; i < MAX_CLIENTS; ++i) {
+        if (!client_sockets[i]) {
+          client_sockets[i] = new_socket;
+          break;
+        }
+      }
+    }
+
+    // Then handle IO operation on some other socket
+    for (size_t i = 0; i < MAX_CLIENTS; ++i) {
+      sd = client_sockets[i];
+      if (FD_ISSET(sd, &readfds)) {
+        valread = read(sd, buf, 1024);
+        if (!valread) {
+          getpeername(sd, (struct sockaddr*)&addr, (socklen_t*)&addrlen);
+          printf("Host disconnected with IP %s and port %d\n",
+                 inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+          close(sd);
+          client_sockets[i] = 0;
+        } else {
+          // Echo back the incoming message
+          buf[valread] = '\0';
+          printf("From client: %s\n", buf);
+          send(sd, buf, strlen(buf), 0);
+        }
+      }
+    }
+  }
+
   delete server;
   delete broker;
+
   return 0;
 }
