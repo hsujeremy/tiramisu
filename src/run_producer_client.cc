ce226b807e6f28c4f6922c3a21bd74ff80d443b1
Jeremy Hsu <jeremyhsu1@gmail.com>
2022-08-11 11:33:01 -0700

    Sending records somewhat work

--- src/run_producer_client.cc
+++ src/run_producer_client.cc
@@ -9,25 +9,6 @@
 #include "producer_client.h"
 
 int main() {
-  // // Tbh we can just make the transactions here for now
-  // ProducerClient* prod = new ProducerClient;
-  // prod->connect_to_server();
-  // if (prod->client_socket < 0) {
-  //   exit(1);
-  // }
-
-  // prod->init_transactions();
-  // prod->begin_transaction();
-  // for (size_t i = 0; i < 10; ++i) {
-  //   prod->send_record(i);
-  // }
-  // prod->commit_transaction();
-  // prod->close_producer();
-  // prod->close_connection();
-
-  // delete prod;
-  // return 0;
-
   ProducerClient* prod = new ProducerClient();
   int r = prod->connect_to_server();
   if (r < 0) {
@@ -36,8 +17,16 @@ int main() {
   }
 
   prod->init_transactions();
+  prod->begin_transaction();
+  for (size_t i = 0; i < 10; ++i) {
+    prod->send_record(i);
+  }
+  prod->close_producer();
 
+  // TODO: Do we also need to close the socket?
   close(prod->client_fd);
+
+  delete prod;
   return 0;
 
 }
