34e6e0f8f5a5ec458ab49fa9f2dab7c4fcd860ff
Jeremy Hsu <jeremyhsu1@gmail.com>
2022-08-25 11:34:04 -0700

    Comment

--- src/common.h
+++ src/common.h
@@ -8,8 +8,13 @@
 #define PORT 8888
 #define DBG 1
 
+// split_strings(str, delim)
+//     Splits `str` into substrings separated by `delim`.
+//     Returns a new vector of substrings.
 std::vector<std::string> split_strings(std::string str, const char delim);
 
+// dbg_printf(debug, format, ...)
+//     Prints the formatted string if `debug` is set to true.
 void dbg_printf(const bool debug, const char* format, ...);
 
 #endif
